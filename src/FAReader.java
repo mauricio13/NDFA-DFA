9b5fa49046219b1d50d2d33e13ea296d10442eff
Jorge González <neoinyawhe@gmail.com>
2013-05-23 20:34:00 -0500

    FAReader corrected

--- src/FAReader.java
+++ src/FAReader.java
@@ -1,3 +1,5 @@
+package pruebas;
+
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -33,8 +35,7 @@ public class FAReader {
 	    try {
 	        String line = br.readLine();
 	        while (line != null) {
-	            line = br.readLine();
-	            String tokens[] = line.split(" ");
+	            String tokens[] = line.split("\t");
 	            String stateId = tokens[0];
 	            String stateType = tokens[1];
 	            String transitionIfZero = tokens[2];
@@ -57,7 +58,7 @@ public class FAReader {
 	            if (stateType.equals("-")){
 	            	insertOrUpdateState(automata, State.StateType.NORMAL, stateId, transitionIfZero, transitionIfOne, transitionIfEpsilon);
 	            }
-	            
+	            line = br.readLine();
 	        }
 	    } finally {
 	        br.close();
@@ -95,15 +96,5 @@ public class FAReader {
 		
 	}
 	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String comps[] = "1,10,2,".split(",");
-		for (String s : comps){
-			System.out.println(s);
-		}
-
-	}
 
 }
